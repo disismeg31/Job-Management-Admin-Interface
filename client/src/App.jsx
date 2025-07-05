8cf48559402bd3ead82725b245159ec67253cce8
Megha KL <megha@nortvis.com>
2025-07-05 14:19:33 +0530

    frontend initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+   
+  return (
+    <>
+
+      <div >
+         
+      </div>
+      <h1>Vite + React</h1>
+       
+      <p className="read-the-docs text-3xl font-bold underline">
+        Full Stack Developer
+      </p>
+    </>
+  )
+}
+
+export default App
