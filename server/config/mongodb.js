a55a24732ddc8c8a4033fa9dde7de39fd722dbd6
Megha KL <megha@nortvis.com>
2025-07-07 19:43:07 +0530

    error handling fix

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 const CONSTANTS = require('./../shared/constants');
 
 const connectDB = async () =>{
-    mongoose.connection.on('connected',()=>console.log('.....Connection Created.....'))
-    await mongoose.connect(`${process.env.MONGODB_URI}/${CONSTANTS.mongoDBName}`)
+    try{
+         console.log("Connecting to MongoDB...");
+         mongoose.connection.on('connected',()=>console.log('.....Connection Created.....'))
+         await mongoose.connect(`${process.env.MONGODB_URI}/${CONSTANTS.mongoDBName}`)
+    }
+    catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1); 
+  }
+   
 }
 
 module.exports = connectDB;
